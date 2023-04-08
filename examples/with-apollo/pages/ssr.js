24db2edd1d3f1d93abd0a51f9de31d4c30a406ea
Tomas Trescak <tomi.trescak@gmail.com>
2023-04-09 09:32:47 +1000

    Update ssr.js
    In your provided example you are using getServerProps, which re-fetches the new data on every page transition. This approach efectively renders the client-side caching useless and most of the Apollo optimisations are not in use. In the updated example I explain this situation and proide an alternative that takes advantage of client side caching on page transitions.

--- examples/with-apollo/pages/ssr.js
+++ examples/with-apollo/pages/ssr.js
@@ -17,6 +17,14 @@ const SSRPage = () => (
   </App>
 )
 
+// DISCLAIMER: Using server side props effectively returns the WHOLE cache 
+//   that was populated during the server side render.
+//   Consequently, you are fetching the data from server on every page transition, 
+//   even if it is already cached on client.
+//   If you want to take advantage of client side cache, you have to use "getInitialProps" method below
+//   But, in that case the data will be fetched from client on page transitions.
+//   Please choos depending on your use case and security requirements.
+
 export async function getServerSideProps() {
   const apolloClient = initializeApollo()
 
@@ -29,5 +37,25 @@ export async function getServerSideProps() {
     props: {},
   })
 }
+
+// ALTERNATIVE approach that takes advantage of client side cache
+//  for apollo and fetches data on client on page transition
+// SSRPage.getInitialProps = async (context) => {
+//  if (typeof window == 'undefined') {
+//    const apolloClient = initializeApollo()
+//
+//    await apolloClient.query({
+//      query: ALL_POSTS_QUERY,
+//      variables: allPostsQueryVars,
+//    })
+// 
+//    // we have to return plain props in getInitialProps
+//    return { 
+//      ...addApolloState(apolloClient, {
+//        props: {},
+//      }).props
+//    }
+//  }
+// }
 
 export default SSRPage
